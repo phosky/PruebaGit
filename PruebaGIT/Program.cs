bfcbdeeca4423a10988ebdade601b2ccdaa1db3e
phosky <carlos.parrilla@vsanywhere.com>
2015-01-22 12:17:47 +0100

    Committing Project

---
+++ PruebaGIT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
